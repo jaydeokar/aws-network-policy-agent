44966057e6a1b9ff0005eb3fe87d7768ccade69d
Jay Deokar <jsdeokar@amazon.com>
2024-02-23 12:09:13 -0800

    Increase number of retries to 3

--- scripts/lib/tests.sh
+++ scripts/lib/tests.sh
@@ -27,7 +27,7 @@ spec:
           command:
             - ./cyclonus
             - generate
-            - --retries=2
+            - --retries=3
             ${IMAGE_REPOSITORY_PARAMETER}
 EOF
 }
@@ -41,7 +41,7 @@ function run_cyclonus_tests(){
     generate_manifest_and_apply
 
     echo "Executing cyclonus suite"
-    kubectl wait --for=condition=complete --timeout=300m -n netpol job.batch/cyclonus || echo "Job timed out after 4 hrs"
+    kubectl wait --for=condition=complete --timeout=300m -n netpol job.batch/cyclonus || echo "Job timed out after 5 hrs"
     kubectl logs -n netpol job/cyclonus > ${DIR}/results.log
 
     kubectl get pods -A -owide
